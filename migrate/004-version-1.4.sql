8f41abb7bf369929e41784eb6b747ea5cf5c25e9
Quentin Kaiser <kaiserquentin@gmail.com>
2013-11-20 22:50:09 +0100

    Remove database name so we can run the script on any database

--- migrate/004-version-1.4.sql
+++ migrate/004-version-1.4.sql
@@ -1,10 +1,10 @@
 # adding new turtles
-INSERT INTO `flatturtle`.`turtle` (`id`, `name`, `type`, `allow_left`, `options`) VALUES (NULL, 'Offers', 'offers', '1', 'data'), (NULL, 'Price list', 'pricelist', '1', 'data'), (NULL, 'Week menu', 'weekmenu', '1', 'data'), (NULL, 'Info', 'info', '0', 'data'), (NULL, 'Calendar', 'calendar', 1, 'url, header');
+INSERT INTO `turtle` (`id`, `name`, `type`, `allow_left`, `options`) VALUES (NULL, 'Offers', 'offers', '1', 'data'), (NULL, 'Price list', 'pricelist', '1', 'data'), (NULL, 'Week menu', 'weekmenu', '1', 'data'), (NULL, 'Info', 'info', '0', 'data'), (NULL, 'Calendar', 'calendar', 1, 'url, header');
 
 
 # replacing power off crons
-UPDATE  `flatturtle`.`job` SET  `javascript` =  'try {Power.enable();} catch (err) {}' WHERE  `job`.`id` =1;
-UPDATE  `flatturtle`.`job` SET  `javascript` =  'try {Power.disable();} catch (err) {}' WHERE  `job`.`id` =2;
+UPDATE  `job` SET  `javascript` =  'try {Power.enable();} catch (err) {}' WHERE  `job`.`id` =1;
+UPDATE  `job` SET  `javascript` =  'try {Power.disable();} catch (err) {}' WHERE  `job`.`id` =2;
 
 # adding zoom cron
-INSERT INTO `flatturtle`.`job` (`id`, `name`, `javascript`) VALUES (NULL, 'zoom', 'try {document.body.style.zoom=0.66666;} catch (err) {}');
+INSERT INTO `job` (`id`, `name`, `javascript`) VALUES (NULL, 'zoom', 'try {document.body.style.zoom=0.66666;} catch (err) {}');
